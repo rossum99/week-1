f068bb1f2c2e57071fad2b9ab6bc75ec593aa339
rossum99 <108015375+rossum99@users.noreply.github.com>
2024-11-27 13:40:09 +0530

    Create arithmetic.c

---
+++ arithmetic.c
@@ -0,0 +1,6 @@
+#inlcude <stdio.h>
+int main()
+{int a,b;
+scanf("%d %d",&a,&b)
+printf("The sum is %d, difference is %d, quotient is %d, product is %d, remainder is %d",a+b,a-b,a/b,a*b,a%b);
+return 0;}
